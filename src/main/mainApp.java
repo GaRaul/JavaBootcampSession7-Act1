eed3c68515b308e8548918f91cf639871b287b3a
Abel-Git2103 <abel_jornet@hotmail.com>
2022-01-19 14:45:13 +0100

    CLASE EJECUTABLE COMPLETADA
    MOSTRAR ELECTRODOMESTICOS POR SEPARADO Y SUMA DEL PRECIO TOTAL

--- src/main/mainApp.java
+++ src/main/mainApp.java
@@ -1,38 +1,90 @@
 package main;
 
 import objetos.Electrodomesticos;
+import objetos.Lavadora;
 import objetos.Television;
 
 public class mainApp {
 
 	public static void main(String[] args) {
-		// . Crearemos una clase llamada Electrodomestico con las siguientes características:
-//		• Sus atributos son precio base, color, consumo energético (letras entre A y F) ypeso. 
-//		Indica que se podrán heredar.
-//		• Por defecto, el color sera blanco, el consumo energético sera F, el precioBase es de 
-//		100 € y el peso de 5 kg. Usa constantes para ello.
-//		• Los colores disponibles son blanco, negro, rojo, azul y gris. No importa si el nombre 
-//		esta en mayúsculas o en minúsculas.
-//		• Los constructores que se implementaran serán
-//		o Un constructor por defecto.
-//		o Un constructor con el precio y peso. El resto por defecto.
-//		o Un constructor con todos los atributos.
 
-		Electrodomesticos elec1 = new Electrodomesticos();
-		Electrodomesticos elec2 = new Electrodomesticos(200, 12.4);
-		Electrodomesticos elec3 = new Electrodomesticos(200, "negro", 'A', 12.4);
-		Electrodomesticos elec4 = new Electrodomesticos(200, "amarillo", 'A', 12.4);
-		Electrodomesticos elec5 = new Electrodomesticos(200, "amarillo", 'J', 12.4);
-		Television tele1 = new Television(100, "amarillo", 'J', 12.4, 41, true);
-		tele1.precioFinal();
-		
-		System.out.println(elec1.toString());
-		System.out.println(elec2.toString());
-		System.out.println(elec3.toString());
-		System.out.println(elec4.toString());
-		System.out.println(elec5.toString());
+		//Crea un array de Electrodomesticos de 10 posiciones.
 
-		System.out.println(tele1.toString());
+		Electrodomesticos[] electrodomesticos = new Electrodomesticos[10];
+
+		//Asigna a cada posición un objeto de las clases anteriores con los valores que
+		//desees.
+
+		electrodomesticos[0] = new Lavadora(200, "amarillo", 'J', 12.4, 40);
+		electrodomesticos[1] = new Television(250, "amarillo", 'J', 12.4, 41, true);
+		electrodomesticos[2] = new Lavadora(160, "amarillo", 'J', 12.4, 20);
+		electrodomesticos[3] = new Lavadora(560, "amarillo", 'J', 12.4, 15);
+		electrodomesticos[4] = new Television(650, "amarillo", 'J', 12.4, 41, true);
+		electrodomesticos[5] = new Lavadora(756, "amarillo", 'J', 12.4, 25);
+		electrodomesticos[6] = new Television(675, "amarillo", 'J', 12.4, 41, true);
+		electrodomesticos[7] = new Television(234, "amarillo", 'J', 12.4, 41, true);
+		electrodomesticos[8] = new Lavadora(545, "amarillo", 'J', 12.4, 55);
+		electrodomesticos[9] = new Television(356, "amarillo", 'J', 12.4, 41, true);
+
+		//Ahora, recorre este array y ejecuta el método precioFinal
+
+		int contTelevisiones = 0;
+		int contLavadoras = 0;
+		double sumaTotal = 0;
+
+		for (int i = 0; i < electrodomesticos.length; i++) {
+			electrodomesticos[i].precioFinal();
+			sumaTotal += electrodomesticos[i].getPrecio();
+
+			if (electrodomesticos[i] instanceof Television) {
+				contTelevisiones++;
+
+			} else {
+				contLavadoras++;
+			}
+		}
+
+		Television[] televisiones = new Television[contTelevisiones];
+		Lavadora[] lavadoras = new Lavadora[contLavadoras];
+
+		contTelevisiones = 0;
+		contLavadoras = 0;
+
+		for (int i = 0; i < electrodomesticos.length; i++) {
+			electrodomesticos[i].precioFinal();
+
+			if (electrodomesticos[i] instanceof Television) {
+				televisiones[contTelevisiones] = (Television) electrodomesticos[i];
+				contTelevisiones++;
+
+			} else {
+				lavadoras[contLavadoras] = (Lavadora) electrodomesticos[i];
+				contLavadoras++;
+			}
+		}
+
+		for (int i = 0; i < lavadoras.length; i++) {
+			System.out.println("El precio de la lavadora " + i + " es: " + lavadoras[i].getPrecio());
+		}
+
+		System.out.println("------");
+
+		for (int i = 0; i < televisiones.length; i++) {
+			System.out.println("El precio de la television " + i + " es: " + televisiones[i].getPrecio());
+		}
+
+		//Deberás mostrar el precio de cada clase, es decir, el precio de todas las
+		//televisiones por un lado, el de las lavadoras por otro y la suma de los
+		//Electrodomesticos (puedes crear objetos Electrodomestico , pero recuerda que
+		//Television y Lavadora también son electrodomésticos). Recuerda el uso operador
+		//instanceof
+
+		//Por ejemplo, si tenemos un
+		//Electrodomestico con un precio final de 300, una
+		//lavadora de 200 y una televisión de 500, el resultado final sera de 1000
+		//(300+200+500) para electrodomésticos, 200 para lavadora y 500 para television.
+		System.out.println("------");
+		System.out.println("Suma total de todos los electrodomesticos = " + sumaTotal);
 
 	}
 }
